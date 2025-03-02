13c6927cdb5653a03d818fc2b9d91d991708a1fa
Hayden Taylor <h.taylor2106@gmail.com>
2025-03-02 11:42:25 -0500

    enable enter key press

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,7 @@ function App(): React.JSX.Element {
               placeholder='Input topic'
               value={input}
               onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => e.key === 'Enter' && handleSubmission()}
               className="input-field"
             />
 
